bc3f305e6de31117e0074c6c23af6da9e6c7221f
App Academy Student <bertie458@gmail.com>
2013-10-16 15:16:18 -0700

    progress on body params

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -2,16 +2,25 @@ require 'uri'
 
 class Params
   def initialize(req, route_params)
+    @params = {}
+    if req.query_string
+      @params.merge!(parse_www_encoded_form(req.query_string))
+    end
+    @params.merge!(parse_www_encoded_form(req.body))
   end
 
   def [](key)
   end
 
   def to_s
+    @params.to_s
   end
 
   private
   def parse_www_encoded_form(www_encoded_form)
+    array = URI.decode_www_form(www_encoded_form)
+    p array
+    Hash[array]
   end
 
   def parse_key(key)
